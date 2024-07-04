62268941123f07ff9a84f2e3be4ddbcb80a51b3e
AFZL210 <tricktomade@gmail.com>
2024-07-05 01:36:40 +0530

    fix worker

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,7 +1,14 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { Job, Worker } from "bullmq";
+import axios from 'axios';
+import { CodeI } from '../common/typedefs/types';
 
 const worker = new Worker(process.env.REDIS_QUEUE_NAME!, async (job: Job) =>  {
-    
+    const data: CodeI = job.data
+    const output = "DUMMY_OUTPUT";
+
+    await axios.post(data.callbackUrl, {
+        output
+    });
 }, { connection: {} });
