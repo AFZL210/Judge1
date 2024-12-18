1a3b9df5ce613e96c7776040678ec391a4e33505
AFZl210 <tricktomade@gmail.com>
2024-12-18 17:33:33 +0530

    setup http server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.HTTP_SERVER_PORT;
+
+app.listen(PORT, () => {
+    console.log(`HTTP Server running on`, `http://localhost:${PORT}`);
+});
