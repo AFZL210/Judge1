9098208bf0d1a4dc45f74a32395ddcbcce6dadf6
AFZl210 <tricktomade@gmail.com>
2024-12-18 19:27:09 +0530

    nit

--- src/server/controllers/code_controller.ts
+++ src/server/controllers/code_controller.ts
@@ -1,5 +1,28 @@
 import { Request, Response, NextFunction } from "express";
+import prisma from '../../common/utils/db';
+import createError, {ErrorI} from "../../common/utils/error";
+import { CodeSchema, Code } from "../../common/typedefs/types";
 
 export const executeCode = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({ok: true})
+  try {
+    const { code } = req.body;
+    const userId = req.get('judge1UserId');
+    CodeSchema.parse(code);
+
+    await prisma.code.create({
+      data: {
+        user: {
+          connect: {
+            id: parseInt(userId!)
+          }
+        },
+        ...code
+      }
+    });
+
+    res.json({data: null, msg: 'Pushed code in execution queue'}).status(201);
+  }
+  catch(e) {
+    next(createError(e as ErrorI));
+  }
 }
