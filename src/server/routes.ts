b5ff9887ce95f76df6049d2c18e4a309fee9c009
AFZl210 <tricktomade@gmail.com>
2024-12-18 19:17:43 +0530

    add auth

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,8 +2,9 @@ import express from 'express';
 const router = express.Router();
 import * as codeController from './controllers/code_controller';
 import * as userController from './controllers/user_controller';
+import * as middleware from './middleware';
 
-router.post('/execute', codeController.executeCode);
+router.post('/execute', middleware.authMiddleware, codeController.executeCode);
 router.post('/createuser', userController.createUser);
 
 export default router;
