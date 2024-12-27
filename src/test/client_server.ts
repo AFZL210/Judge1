b082696b218e1e35576bbb29458386f7277a7126
AFZl210 <tricktomade@gmail.com>
2024-12-27 19:11:01 +0530

    fix

--- src/test/client_server.ts
+++ src/test/client_server.ts
@@ -22,7 +22,18 @@ app.post("/catch-output", (req: Request, res: Response) => {
 app.get("/metrics", async (req: Request, res: Response) => {
   const codes = await prisma.code.findMany();
 
-  res.json(codes);
+  let timeSum = 0;
+  let len = codes.length;
+
+  for(let i=0; i<len; i++) {
+    timeSum += codes[i].execution_time;
+  }
+
+  res.json({
+    number_of_code_executed: len,
+    average_execution_time: (timeSum/len),
+    longest_execution_time: Math.max(...(codes.map(code => code.execution_time)))
+  });
 });
 
 app.listen(PORT, () => {
